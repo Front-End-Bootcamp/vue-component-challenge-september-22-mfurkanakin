7842e002b0b6e6c88f0625861f9d6d47c09ec695
mfurkanakin <mfakin@yahoo.com>
2022-09-24 02:15:17 +0300

    not yet

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,51 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import { objectToString } from "@vue/shared";
+import {onMounted} from "vue";
+
+async function fetchData() {
+	const allData = await fetch('https://raw.githubusercontent.com/Front-End-Bootcamp/vue-bootcamp/d6f881aec77f15a4107eafc10f6a91e90f9268a4/homework%201/data.json')
+		.then(data => data.json())
+		.then(response => response);
+		return allData
+
+}
+function getGroupNames(data) {
+		const groupNames = data.map(person => person.group);
+		const uniqueNames = [...new Set(groupNames)]
+		console.log('groupNames', uniqueNames)
+		return uniqueNames;
+}
+function filterByGroups(data){
+	const groups = data.reduce((currentData,person) => {
+		currentData[person.group] = []
+		return currentData;
+	},{});
+
+	Object.keys(groups).forEach(group => {
+		const students = data.filter(person => person.group == group);
+		groups[group] = students;
+	})
+	return groups;
+}
+
+
+
+// const DATA = fetchData();
+// getGroupNames(DATA);
+//birbirini beklemedi await neden bekletmedi yukarda data.map not defined hatası verdi?
+
+fetchData().then((response)=>(
+	filterByGroups(response)
+));
+
+
 </script>
 
 <template>
 	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
-
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
